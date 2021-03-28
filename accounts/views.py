6e36122000263f53ca9589df5019726f333743cc
Ali <aliabbasi.95@yahoo.com>
2021-03-29 02:02:02 +0430

    use serializer in register view

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,15 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from accounts.serializers import UserRegistrationSerializer
+
 
 class UserRegistrationView(APIView):
+    serializer_class = UserRegistrationSerializer
+
     def post(self, request):
-        return Response(status=status.HTTP_200_OK)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
