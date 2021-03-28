16cbf6de03eb61011030f3d580771821cf0b8a72
Ali <aliabbasi.95@yahoo.com>
2021-03-29 01:58:35 +0430

    UserRegistrationSerializer in accounts/serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+import django.contrib.auth.password_validation as validators
+
+User = get_user_model()
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=User.objects.all())])
+    username = serializers.CharField(max_length=32, validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(validators=[validators.validate_password])  # has changed
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'],
+                                        validated_data['password'])
+        return user
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'password')
