2749356943a224e387af23a5c5bed39e4ccc66cb
Ali <aliabbasi.95@yahoo.com>
2021-04-26 02:23:00 +0430

    add test_register_user

--- accounts/tests.py
+++ accounts/tests.py
@@ -11,3 +11,24 @@ class AccountsTest(APITestCase):
 
         # URL for creating an account.
         self.register_url = reverse('account_register')
+
+    def test_register_user(self):  # new
+        """
+        Ensure we can create a new user and a valid token is created with it.
+        """
+        data = {
+            'username': 'foobar',
+            'email': 'foobar@example.com',
+            'password': 'Somepassword1@'
+        }  # new
+
+        response = self.client.post(self.register_url, data, format='json')  # new
+
+        # We want to make sure we have two users in the database..
+        self.assertEqual(User.objects.count(), 2)  # new
+        # And that we're returning a 201 created code.
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # new
+        # Additionally, we want to return the username and email upon successful creation.
+        self.assertEqual(response.data['username'], data['username'])  # new
+        # self.assertEqual(response.data['email'], data['email'])
+        self.assertFalse('password' in response.data)  # new
