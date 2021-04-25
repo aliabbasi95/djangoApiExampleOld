47b134a98f3effa59e89736701f7a5cbe110f5c4
Ali <aliabbasi.95@yahoo.com>
2021-04-26 02:03:58 +0430

    create AccountsTest and setup

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AccountsTest(APITestCase):
+    def setUp(self):
+        # We want to go ahead and originally create a user.
+        self.test_user = User.objects.create_user('testuser', 'test@example.com', 'testpassword')
+
+        # URL for creating an account.
+        self.register_url = reverse('account_register')
