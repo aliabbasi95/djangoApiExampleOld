4e0a01bae4482d88eaf0cef4be7f0ed19960eee1
Ali <aliabbasi.95@yahoo.com>
2021-05-25 00:54:14 +0430

    add test_register_user_with_password_has_not_upper

--- accounts/tests.py
+++ accounts/tests.py
@@ -89,3 +89,21 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         # And check password in response data equal 1
         self.assertEqual(len(response.data['password']), 1)
+
+    def test_register_user_with_password_has_not_upper(self):
+        """
+        Ensure user is not created for password has not upper.
+        """
+        data = {
+            'username': 'foobar',
+            'email': 'foobarbaz@example.com',
+            'password': 'testpassword1@'
+        }
+
+        response = self.client.post(self.register_url, data, format='json')
+        # We want to make sure we have one user in the database..
+        self.assertEqual(User.objects.count(), 1)
+        # And that we're returning a 400 created code.
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # And check password in response data equal 1
+        self.assertEqual(len(response.data['password']), 1)
