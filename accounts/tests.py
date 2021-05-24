61ed429467e669b85d15e34e8a208fbf5474d9f0
Ali <aliabbasi.95@yahoo.com>
2021-05-25 00:56:33 +0430

    add test_register_user_with_too_long_username

--- accounts/tests.py
+++ accounts/tests.py
@@ -143,3 +143,21 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         # And check password in response data equal 1
         self.assertEqual(len(response.data['password']), 1)
+
+    def test_register_user_with_too_long_username(self):
+        """
+        Ensure user is not created for username is long.
+        """
+        data = {
+            'username': 'foo' * 30,
+            'email': 'foobarbaz@example.com',
+            'password': 'Somepassword1@'
+        }
+
+        response = self.client.post(self.register_url, data, format='json')
+        # We want to make sure we have one user in the database..
+        self.assertEqual(User.objects.count(), 1)
+        # And that we're returning a 400 created code.
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # And check username in response data equal 1
+        self.assertEqual(len(response.data['username']), 1)
