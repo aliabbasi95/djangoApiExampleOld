2fb691895c875f0f777aeabeaead100b964fe64c
Ali <aliabbasi.95@yahoo.com>
2021-05-25 00:53:38 +0430

    add test_register_user_with_password_has_not_number

--- accounts/tests.py
+++ accounts/tests.py
@@ -72,3 +72,20 @@ class AccountsTest(APITestCase):
         # And check password in response data equal 1
         self.assertEqual(len(response.data['password']), 1)
 
+    def test_register_user_with_password_has_not_number(self):
+        """
+        Ensure user is not created for password has not number.
+        """
+        data = {
+            'username': 'foobar',
+            'email': 'foobarbaz@example.com',
+            'password': 'TestPassword@'
+        }
+
+        response = self.client.post(self.register_url, data, format='json')
+        # We want to make sure we have one user in the database..
+        self.assertEqual(User.objects.count(), 1)
+        # And that we're returning a 400 created code.
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # And check password in response data equal 1
+        self.assertEqual(len(response.data['password']), 1)
