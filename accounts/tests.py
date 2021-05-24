2276cc97daa6fb4753ede4c4418527eaf7fec831
Ali <aliabbasi.95@yahoo.com>
2021-05-25 00:52:32 +0430

    add test_register_user_with_no_password

--- accounts/tests.py
+++ accounts/tests.py
@@ -49,7 +49,26 @@ class AccountsTest(APITestCase):
         response = self.client.post(self.register_url, data, format='json')
         # We want to make sure we have one user in the database..
         self.assertEqual(User.objects.count(), 1)
-        # And that we're returning a 201 created code.
+        # And that we're returning a 400 created code.
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         # And check length of password in response data equal 1
         self.assertEqual(len(response.data['password']), 1)
+
+    def test_register_user_with_no_password(self):
+        """
+        Ensure user is not created for no password.
+        """
+        data = {
+            'username': 'foobar',
+            'email': 'foobarbaz@example.com',
+            'password': ''
+        }
+
+        response = self.client.post(self.register_url, data, format='json')
+        # We want to make sure we have one user in the database..
+        self.assertEqual(User.objects.count(), 1)
+        # And that we're returning a 400 created code.
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # And check password in response data equal 1
+        self.assertEqual(len(response.data['password']), 1)
+
