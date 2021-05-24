60ea126c61bd4cf3672b2240175cc36c6629268e
Ali <aliabbasi.95@yahoo.com>
2021-05-25 01:22:04 +0430

    add test_register_user_with_invalid_email

--- accounts/tests.py
+++ accounts/tests.py
@@ -197,3 +197,21 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         # And check username in response data equal 1
         self.assertEqual(len(response.data['username']), 1)
+
+    def test_register_user_with_invalid_email(self):
+        """
+        Ensure user is not created for invalid email.
+        """
+        data = {
+            'username': 'foobar',
+            'email': 'testing',
+            'passsword': 'Somepassword1@'
+        }
+
+        response = self.client.post(self.register_url, data, format='json')
+        # We want to make sure we have one user in the database..
+        self.assertEqual(User.objects.count(), 1)
+        # And that we're returning a 400 created code.
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # And check email in response data equal 1
+        self.assertEqual(len(response.data['email']), 1)
