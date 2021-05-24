c62e28f45de82bf28afdf335fbe029e94709111a
Ali <aliabbasi.95@yahoo.com>
2021-05-25 00:57:42 +0430

    add test_register_user_with_preexisting_username

--- accounts/tests.py
+++ accounts/tests.py
@@ -179,3 +179,21 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         # And check username in response data equal 1
         self.assertEqual(len(response.data['username']), 1)
+
+    def test_register_user_with_preexisting_username(self):
+        """
+        Ensure user is not created for preexisting username.
+        """
+        data = {
+            'username': 'testuser',
+            'email': 'foobarbaz@example.com',
+            'password': 'Somepassword1@'
+        }
+
+        response = self.client.post(self.register_url, data, format='json')
+        # We want to make sure we have one user in the database..
+        self.assertEqual(User.objects.count(), 1)
+        # And that we're returning a 400 created code.
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # And check username in response data equal 1
+        self.assertEqual(len(response.data['username']), 1)
